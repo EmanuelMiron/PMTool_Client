4432ca12e15143b6b1582fae4ea9d31820d2f2a1
EmanuelMiron <emanuel_miron6@gmail.com>
2020-11-29 18:27:21 +0100

    Created Navabar Component

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,10 +1,33 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+
+// Material UI Components
+import AppBar from '@material-ui/core/AppBar';
+import ToolBar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles(theme => ({
+    title: {
+        flexGrow: 1,
+    },
+    button : {
+        marginRight: '0.5rem'
+    }
+}))
 
 const Navbar = () => {
+    const classes = useStyles();
     return (
-        <div>
-            <h1>Navigation</h1>
-        </div>
+        <AppBar position="absolute">
+            <ToolBar>
+                <Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title}>
+                    PM Tool
+                </Typography>
+                <Button className={classes.button} variant="contained" href="/login">Login</Button> 
+                <Button variant="contained" color="secondary" href="/register">Register</Button> 
+            </ToolBar>
+        </AppBar>
     )
 }
 
