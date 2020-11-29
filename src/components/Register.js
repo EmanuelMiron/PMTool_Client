5d18e452f7a62865a5aa520f971ddf5f0727750c
EmanuelMiron <emanuel_miron6@gmail.com>
2020-11-29 19:48:07 +0100

    Created Register Form

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,12 +1,124 @@
-import React from 'react'
-import Navbar from './layout/Navbar'
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+// Material UI Components
+import Container from '@material-ui/core/Container';
+import Avatar from '@material-ui/core/Avatar';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Link from '@material-ui/core/Link';
+
+// Material UI Icons
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+
+const useStyles = makeStyles((theme) => ({
+	paper: {
+		marginTop: theme.spacing(10),
+		display: 'flex',
+		flexDirection: 'column',
+		alignItems: 'center',
+	},
+	avatar: {
+		margin: theme.spacing(1),
+		backgroundColor: theme.palette.secondary.main,
+	},
+	form: {
+		width: '100%',
+		marginTop: theme.spacing(3),
+	},
+	submit: {
+		margin: theme.spacing(3, 0, 2),
+	},
+}));
 
 const Register = () => {
-    return (
-        <>
-            <h1>Register</h1>
-        </>
-    )
-}
+	const classes = useStyles();
+
+	return (
+		<Container component="main" maxWidth="xs">
+			<div className={classes.paper}>
+				<Avatar className={classes.avatar}>
+					<LockOutlinedIcon />
+				</Avatar>
+				<Typography component="h1" variant="h4">
+					Register
+				</Typography>
+				<form className={classes.form}>
+					<Grid container spacing={2}>
+						<Grid item xs={12} sm={6}>
+							<TextField
+								autoComplete="fname"
+								name="firstName"
+								variant="outlined"
+								required
+								fullWidth
+								id="firstName"
+								label="First Name"
+								autoFocus
+							/>
+						</Grid>
+						<Grid item xs={12} sm={6}>
+							<TextField
+								variant="outlined"
+								required
+								fullWidth
+								id="lastName"
+								label="Last Name"
+								name="lastName"
+								autoComplete="lname"
+							/>
+						</Grid>
+						<Grid item xs={12}>
+							<TextField
+								variant="outlined"
+								required
+								fullWidth
+								id="email"
+								label="Email Address"
+								name="email"
+								autoComplete="email"
+							/>
+						</Grid>
+						<Grid item xs={12}>
+							<TextField
+								variant="outlined"
+								required
+								fullWidth
+								name="password"
+								label="Password"
+								type="password"
+								id="password"
+								autoComplete="current-password"
+							/>
+						</Grid>
+						<Grid item xs={12}>
+							<TextField
+								variant="outlined"
+								required
+								fullWidth
+								name="confirmPassword"
+								label="Confirm Password"
+								type="password"
+								id="confirmPassword"
+							/>
+						</Grid>
+					</Grid>
+					<Button type="submit" fullWidth variant="contained" color="secondary" className={classes.submit}>
+						Register
+					</Button>
+                    <Grid container justify='flex-end'>
+                        <Grid item>
+                            <Link href="/login" variant='body2'>
+                                Already have an account? Login
+                            </Link>
+                        </Grid>
+                    </Grid>
+				</form>
+			</div>
+		</Container>
+	);
+};
 
 export default Register;
