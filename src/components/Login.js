7b45527a572176936b48460e0881a2416cc00d66
EmanuelMiron <emanuel_miron6@gmail.com>
2020-11-29 18:32:05 +0100

    Created Login and Register components

---
+++ src/components/Login.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './layout/Navbar'
+
+const Login = () => {
+    return (
+        <>
+            <Navbar />
+            <h1>Login</h1>
+        </>
+    )
+}
+
+export default Login
---
+++ src/components/Register.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './layout/Navbar'
+
+const Register = () => {
+    return (
+        <>
+            <Navbar />
+            <h1>Register</h1>
+        </>
+    )
+}
+
+export default Register;
