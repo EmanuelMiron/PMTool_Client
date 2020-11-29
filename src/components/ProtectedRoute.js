18bf750bf8401b7186847e2f72e38cc213581371
EmanuelMiron <emanuel_miron6@gmail.com>
2020-11-29 18:58:59 +0100

    Added ProtectedRoute Component

---
+++ src/components/ProtectedRoute.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Redirect, Route } from 'react-router-dom';
+
+const isAuthenticated = () => {
+    const user = JSON.parse(localStorage.getItem('user'));
+    return (user && user.accessToken) ? true : false;
+}
+
+const ProtectedRoute = ({ component: Component, ...rest }) => (
+	<Route
+		{...rest}
+		render={(props) => (isAuthenticated ? <Component {...props} /> : <Redirect to="/login" />)}
+	/>
+);
+
+export default ProtectedRoute;
