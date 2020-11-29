832e179bf5d87f9afbe4ddcce25799a28d1bfdcc
EmanuelMiron <emanuel_miron6@gmail.com>
2020-11-29 19:09:11 +0100

    Implement Routes and ProtectedRoute

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import CssBaseLine from '@material-ui/core/CssBaseline';
 import Dashboard from './components/layout/Dashboard';
+import Login from './components/Login';
+import Register from './components/Register';
+import { BrowserRouter as Router, Route} from "react-router-dom";
+import ProtectedRoute from './components/ProtectedRoute';
 
 const  App = () => {
   return (
     <>
       <CssBaseLine />
-      <Dashboard />
+      <Router>
+        <ProtectedRoute path="/" component={Dashboard} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+      </Router>
     </>
   );
 }
